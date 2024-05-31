ab3d9c00f0746cedd8b326d8a7c971f8a39cfd97
GrishaGogichashvili <165365303+GrishaGogichashvili@users.noreply.github.com>
2024-05-31 23:56:14 +0400

    Add files via upload

---
+++ level 5/classwork.py
@@ -0,0 +1,7 @@
+cat_name1 = "gela"
+cat_name2 = "zaza"
+cat_name3 =  "gio"
+cat_name4 = "temura"
+cat_name5 = "murtazi"
+
+print("first cat's name is " + cat_name1 + " second cat's name is " + cat_name2 + " third cat's name is " + cat_name3 + " fourth cat's name is " + cat_name4 + " fifth cat's name is " + cat_name5) 
