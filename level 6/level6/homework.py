772cc293a34ac914f6f3a3fd55f1cf221fcd3ef8
GrishaGogichashvili <165365303+GrishaGogichashvili@users.noreply.github.com>
2024-06-02 11:58:40 +0400

    Add files via upload

---
+++ level 6/level6/homework.py
@@ -0,0 +1,15 @@
+
+
+
+#creating an account
+
+num1 = input("please enter your name:  ")
+num2 = input("please enter your last name:  ")
+num3 = input("please enter your password:  ")
+num4 = input("please enter your email or phone number:  ")
+
+print(int(num1) + int(num2) + int(num3) + int(num4))
+
+print(f"your name is {num1}" + "your last name is {num2}" + "your password is {num3}" + "your gmail or number is {num4")
+
+
