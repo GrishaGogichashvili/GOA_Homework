427ff411d66e544c932e329c48e13953867ba4bd
GrishaGogichashvili <165365303+GrishaGogichashvili@users.noreply.github.com>
2024-08-08 16:20:32 +0400

    Add files via upload

---
+++ level 14/homework.py
@@ -0,0 +1,19 @@
+# 1 დავალება
+
+sum2 = 1
+
+for i in range(1, 100):
+    sum2 = sum2 + i  #sum2 += i
+    print(sum2)
+
+# 2  დავალება
+
+i = 1
+sum1 = 0
+
+for i in range(1000):
+    if i in range(500,600):
+        pass
+    sum1 += i
+
+print(sum1)
